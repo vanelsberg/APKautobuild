b7775ac4c38919f692772d021d58ef2247bf7fdb
vanelsberg <van.elsberg@outlook.com>
2020-11-22 22:24:43 +0100

    Switch to image version tag 0.1.1

--- build_AAPS.cmd
+++ build_AAPS.cmd
@@ -9,6 +9,4 @@ set data_mount=/data
 set data_local=%~dp0%data
 
 rem Run container in interactive mode
-echo "docker run --rm -v "%data_local%:%data_mount%" -it theod00r/tvedockerrepo:0.1.0 %1"
-
-docker run --rm -v "%data_local%:%data_mount%" -it theod00r/tvedockerrepo:0.1.0 %1
+docker run --rm -v "%data_local%:%data_mount%" -it theod00r/tvedockerrepo:0.1.1 %1
--- build_AAPS.sh
+++ build_AAPS.sh
@@ -7,4 +7,4 @@ data_mount=/data
 data_local=$(pwd)/data
 
 # Run container in interactive mode
-docker run --rm -v $data_local:$data_mount -it theod00r/tvedockerrepo:0.1.0 $1
+docker run --rm -v $data_local:$data_mount -it theod00r/tvedockerrepo:0.1.1 $1
