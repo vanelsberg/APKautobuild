f20a40dd41df3a48beff9328cba1f886e8d7ab74
vanelsberg <van.elsberg@outlook.com>
2020-11-29 17:22:58 +0100

    Renamed images name, pull from Azure

--- build_AAPS.cmd
+++ build_AAPS.cmd
@@ -7,6 +7,8 @@ rem Building from Windows 10 command line:
 rem Mounting local data on container
 set data_mount=/data
 set data_local=%~dp0%data
+set image=devtest01.azurecr.io/aapsbuilder:0.1.1
+
 
 rem Run container in interactive mode
-docker run --rm -v "%data_local%:%data_mount%" -it theod00r/tvedockerrepo:0.1.1 %1
+docker run --rm -v "%data_local%:%data_mount%" -it %imaeg% %1
--- build_AAPS.sh
+++ build_AAPS.sh
@@ -5,6 +5,7 @@
 # Mounting local data on container
 data_mount=/data
 data_local=$(pwd)/data
+image=devtest01.azurecr.io/aapsbuilder:0.1.1
 
 # Run container in interactive mode
-docker run --rm -v $data_local:$data_mount -it theod00r/tvedockerrepo:0.1.1 $1
+docker run --rm -v $data_local:$data_mount -it $image $1
