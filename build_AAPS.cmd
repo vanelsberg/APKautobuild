6817466d6d23c3a4b072b364523c86ec0741e24c
vanelsberg <van.elsberg@outlook.com>
2020-12-02 14:42:23 +0100

    Updated to public image on Docker hub

--- build_AAPS.cmd
+++ build_AAPS.cmd
@@ -7,8 +7,9 @@ rem Building from Windows 10 command line:
 rem Mounting local data on container
 set data_mount=/data
 set data_local=%~dp0%data
-set image=devtest01.azurecr.io/aapsbuilder:0.1.1
 
+rem Docker image (public access)
+set image=theod00r/aapsbuilder:0.1.1
 
 rem Run container in interactive mode
 docker run --rm -v "%data_local%:%data_mount%" -it %image% %1
--- build_AAPS.sh
+++ build_AAPS.sh
@@ -5,7 +5,8 @@
 # Mounting local data on container
 data_mount=/data
 data_local=$(pwd)/data
-image=devtest01.azurecr.io/aapsbuilder:0.1.1
+
+image=theod00r/aapsbuilder:0.1.1
 
 # Run container in interactive mode
 docker run --rm -v $data_local:$data_mount -it $image $1
