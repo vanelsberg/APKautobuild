8aad1c95c727cf612c7f00d9c19d612b946f3633
vanelsberg <van.elsberg@outlook.com>
2021-10-21 20:38:25 +0200

    Publishing the apk

--- publish.sh
+++ publish.sh
@@ -5,8 +5,12 @@ source $(pwd)/config.build
 
 if [ "$(ls -A $data_local/output)" ];
 then
+  # Hint: create a symbolic link to a onedrive directory:
+  # $ ln -s /mnt/c/Users/...../OneDrive/Distrib/APKautobuild data/onedrive
   echo "Copying files to OneDrive..."
-  cp $data_local/output/* $data_local/onedrive/
+  dstdir="$data_local/onedrive/$(date '+%Y%m%d_%H%M')"
+  mkdir $dstdir
+  cp $data_local/output/* $dstdir/.
 else
   echo "Nothing to do..."
 fi
