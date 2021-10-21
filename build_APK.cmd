9b926cce7dac358fe3f54b761658360533f98201
vanelsberg <van.elsberg@outlook.com>
2021-10-21 21:10:44 +0200

    Added Docker memory limit parameter

--- build_APK.cmd
+++ build_APK.cmd
@@ -7,11 +7,11 @@ rem Building from Windows 10 command line:
 rem Mounting local data on container
 set data_mount=/data
 set data_local=%~dp0%data
-set docker_runparam="--maxmemory=4g"
+set docker_runparam="--memory=4g"
 
 
 rem Docker image (public access)
 set image=theod00r/apkbuilder:1.0.0
 
 rem Run container in interactive mode
-docker run --rm $docker_runparam --name AAPSbuilder -v "%data_local%:%data_mount%" -it %image% %1
+docker run --rm %docker_runparam% --name AAPSbuilder -v "%data_local%:%data_mount%" -it %image% %1
