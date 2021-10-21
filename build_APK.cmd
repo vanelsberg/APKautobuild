d472786af15fe592c5cbd504a7d61bf035fddcfa
vanelsberg <van.elsberg@outlook.com>
2021-10-21 20:39:06 +0200

    Added run paramaters

--- build_APK.cmd
+++ build_APK.cmd
@@ -7,9 +7,11 @@ rem Building from Windows 10 command line:
 rem Mounting local data on container
 set data_mount=/data
 set data_local=%~dp0%data
+set docker_runparam="--maxmemory=4g"
+
 
 rem Docker image (public access)
 set image=theod00r/apkbuilder:1.0.0
 
 rem Run container in interactive mode
-docker run --rm -v "%data_local%:%data_mount%" -it %image% %1
+docker run -rm $docker_runparam --name AAPSbuilder --rm -v "%data_local%:%data_mount%" -it %image% %1
