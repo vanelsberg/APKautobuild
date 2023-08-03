06009054ad34d78a0d1a3cc6bf201873f5de1f42
vanelsberg <van.elsberg@outlook.com>
2023-08-03 15:58:56 +0200

    Build using moiunted docker volumes volAAPSgit and volAAPSgradle to persist previous build results. Added commandline options: --help, --clean

--- build_APK.sh
+++ build_APK.sh
@@ -5,8 +5,32 @@
 # Loading config
 source $(pwd)/config.build
 
+# Parse optional first parameter
+case "$1" in
+  --help)
+      echo "Use: '$0 --clean' to build from start"
+      exit
+      ;;
+  --clean)
+      # ignore first parameter from here
+      shift
+      # Remove local Docker build volumes
+      echo -e "===> Clean for full rebuild..."
+      docker volume rm volAAPSgit
+      docker volume rm volAAPSgradle
+      echo -e "===> Cleaned!\n"
+      ;;
+  *)
+      echo -e "\n===> Executing update build...\n"
+      ;;
+esac
+
+mount_volAAPSgradle="-v volAAPSgradle:/root/.gradle"
+mount_volAAPSgit="-v volAAPSgit:/user/src/asbuilder/AndroidAPS"
+mount_data="-v $data_local:$data_mount"
+
 # Run container in interactive mode
-cmd="docker run $docker_runparam --rm --name AAPSbuilder -v $data_local:$data_mount -it $image $1"
+cmd="docker run $docker_runparam --rm --name AAPSbuilder ${mount_volAAPSgradle} ${mount_volAAPSgit} ${mount_data} -it $image $1"
 
 echo $cmd
 eval $cmd
