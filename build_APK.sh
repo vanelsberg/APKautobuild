b57efb3270b35f8587927a4bf539ef8103171ceb
vanelsberg <vanelsberg@outlook.com>
2024-01-10 20:56:18 +0100

    Persist git & gradle: add option --clean, --rebuilds

--- build_APK.sh
+++ build_APK.sh
@@ -8,7 +8,9 @@ source $(pwd)/config.build
 # Parse optional first parameter
 case "$1" in
   --help)
-      echo "Use: '$0 --clean' to build from start"
+      echo "Use: '$0 [option]'  to build from start"
+      echo "--clean    full rebuild"
+      echo "--rebuild  Git code rebuild (reclone)"
       exit
       ;;
   --clean)
@@ -18,19 +20,27 @@ case "$1" in
       echo -e "===> Clean for full rebuild..."
       docker volume rm volAAPSgit
       docker volume rm volAAPSgradle
-      echo -e "===> Cleaned!\n"
+      echo -e "===> Gradle & Git Cleaned!\n"
+      ;;
+  --rebuild)
+      # ignore first parameter from here
+      shift
+      # Remove local Docker build volumes
+      echo -e "===> Clean for Git rebuild..."
+      docker volume rm volAAPSgit
+      echo -e "===> Git cleaned!\n"
       ;;
   *)
       echo -e "\n===> Executing update build...\n"
       ;;
 esac
 
-mount_volAAPSgradle="-v volAAPSgradle:/root/.gradle"
-mount_volAAPSgit="-v volAAPSgit:/user/src/asbuilder/AndroidAPS"
-mount_data="-v $data_local:$data_mount"
-
 # Run container in interactive mode
-cmd="docker run $docker_runparam --rm --name AAPSbuilder ${mount_volAAPSgradle} ${mount_volAAPSgit} ${mount_data} -it $image $1"
+cmd="docker run $docker_runparam --rm --name AAPSbuilder \
+--mount source=volAAPSgradle,target=/root/.gradle \
+--mount source=volAAPSgit,target=/user/src/asbuilder/AndroidAPS \
+--mount type=bind,source=$data_local,target=$data_mount \
+-it $image $1"
 
 echo $cmd
 eval $cmd
