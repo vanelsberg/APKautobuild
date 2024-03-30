62b7b83d5a30c862a470225f3976ec1b791585af
vanelsberg <vanelsberg@outlook.com>
2024-03-30 21:30:11 +0100

    Parameterize builds directory

--- publish.sh
+++ publish.sh
@@ -7,9 +7,9 @@ if [ "$(ls -A $data_local/output)" ];
 then
   # Hint: create a symbolic link to a onedrive directory:
   # $ ln -s /mnt/c/Users/...../OneDrive/Distrib/APKautobuild data/onedrive
-  echo "Copying files to OneDrive..."
-  dstdir="$data_local/onedrive/${BUILDENV}$(date '+%Y%m%d_%H%M')"
-  mkdir $dstdir
+  echo "Copying files to $publishdir..."
+  dstdir="$data_local/$publishdir/${BUILDENV}$(date '+%Y%m%d_%H%M')"
+  mkdir -p $dstdir
   cp $data_local/output/* $dstdir/.
 else
   echo "Nothing to do..."
